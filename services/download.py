ef79dd51d41dbc39a33e1e1868ea5a59a20bbed7
Votati <tomasbottari300@gmail.com>
2024-02-04 06:30:07 -0300

    ahora guarda los archivos con un nombre random

--- services/download.py
+++ services/download.py
@@ -1,17 +1,26 @@
 from yt_dlp import YoutubeDL
 import os
+import random
+import string
 
 def download_video(url):
 	current_path = os.path.dirname(__file__)
 	output_directory = f"{current_path}/../storage"
 
+	fileName = generateRandomString()
+
 	ydlp_config = {
-		"outtmpl": f"{output_directory}/%(title)s.%(ext)s",
+		"outtmpl": f"{output_directory}/{fileName}.%(ext)s",
 		"cookiefile": "cookie.txt"
 	}
 
 	with YoutubeDL(ydlp_config) as ydl:
 		info = ydl.extract_info(url, download=True)
-		fileName = f"{info['title']}.{info['ext']}"
+		fileName = f"{fileName}.{info['ext']}"
 		return f"/api/get_video/{fileName}"
 
+def generateRandomString():
+    characters = string.ascii_letters + string.digits
+    length = 10
+    random_string  = ''.join(random.choice(characters) for _ in range(length))
+    return random_string 
